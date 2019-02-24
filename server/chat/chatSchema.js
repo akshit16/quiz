89f19138cfb33be5d1abf96fb71e96f8cb76da8c
akshit16 <jain.akshit16@gmail.com>
2019-02-24 18:13:07 +0530

    chat schema added

---
+++ server/chat/chatSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+autoIncrement = require('mongoose-auto-increment');
+var connection = mongoose.createConnection("mongodb://localhost/quizapp");
+autoIncrement.initialize(connection);
+
+const chatSchema = new Schema({
+  user1: {
+    type: String
+  },
+  user2: {
+    type: String
+  },
+  group: [{
+    type: String
+  }],
+  chat: [{
+   type:String
+  }],
+  createdAt: {
+    type: String,
+    required: true,
+    default: Date.now
+  }
+});
+
+chatSchema.plugin(autoIncrement.plugin, 'id');
+
+
+module.exports = mongoose.model('chat', chatSchema);
