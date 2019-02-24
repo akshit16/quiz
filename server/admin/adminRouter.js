109a317e57d70ff05ae559a2c51e8be949e6ece7
akshit16 <jain.akshit16@gmail.com>
2019-02-24 15:46:51 +0530

    tr

--- server/admin/adminRouter.js
+++ server/admin/adminRouter.js
@@ -1,11 +1,11 @@
-let express = require('express');
-let router = express.Router()
-adminSchema = require('./adminSchema')
-var adminController = require('./adminController');
-const bodyParser = require('body-parser');
+// let express = require('express');
+// let router = express.Router()
+// adminSchema = require('./adminSchema')
+// var adminController = require('./adminController');
+// const bodyParser = require('body-parser');
 
 
-var admin = new adminSchema()
+// var admin = new adminSchema()
 
 
-module.exports = router;
+// module.exports = router;
